7fed2549c87b996a92fff31534583e6c45d93bc1
周杰 <zhoujie0083@gmail.com>
2024-11-04 17:15:42 +0900

    后台逻辑开发 与 页面的调整

--- gradio_server.py
+++ gradio_server.py
@@ -3,24 +3,36 @@ import time
 
 import gradio as gr
 
+from vectors_retrieval import save_vectors_db, init_chain
+
 
 def do_user(user_message, history):
     history.append((user_message, None))
     return '', history
 
 
-def do_it(history):
+def do_it(history,):
     print(history)
-    responses = [
-        "メッセージありがとうございます！",
-        "とても興味深いです！",
-        "どう答えればいいのかわかりません。",
-        "他にご質問はありますか？",
-        "できるだけ早くご連絡させていただきます。",
-        "あなたとコミュニケーションを取ることができて嬉しいです！",
-    ]
+    question = history[-1][0]
+    res = robot.invoke({'input': question})
+    resp = res['answer']
 
-    resp = random.choice(responses)
+    if not resp:
+        resp = 'すみません。この問題は回答出来ません、スタッフに聞いてください。'
+
+
+
+    #ソース動作疎通
+    # responses = [
+    #     "メッセージありがとうございます！",
+    #     "とても興味深いです！",
+    #     "どう答えればいいのかわかりません。",
+    #     "他にご質問はありますか？",
+    #     "できるだけ早くご連絡させていただきます。",
+    #     "あなたとコミュニケーションを取ることができて嬉しいです！",
+    # ]
+    #
+    # resp = random.choice(responses)
 
 
     history[-1][1] = ''
@@ -31,21 +43,35 @@ def do_it(history):
         time.sleep(0.1)
         yield history
 
-css = """
-#bgc {background-color: #7FFFD4}
-.feedback textarea {font-size: 24px !important}
-"""
+def run_gradio():
 
-# Blocks：
-with gr.Blocks(title='日本の税金AI_ChatRobot', css=css) as instance:
-    gr.Label('日本の税金AI_ChatRobot', container=False)
-    chatbot = gr.Chatbot(height=550, placeholder='<strong>AI ChatRobot</strong><br> こんにちは、何かお手伝いできますか?',type='messages')
-    msg = gr.Textbox(placeholder='質問を入力してください！', elem_classes='feedback', elem_id='bgc')
-    clear = gr.ClearButton(value='会話の記録をクリア', components=[msg, chatbot])
+    css = """
+    #bgc {background-color: #7FFFD4}
+    .feedback textarea {font-size: 24px !important}
+    """
+
+    # Blocks：
+    with gr.Blocks(title='日本の税金AI_ChatRobot', css=css) as instance:
+        gr.Label('日本の税金AI_ChatRobot', container=False)
+        chatbot = gr.Chatbot(height=550, placeholder='<strong>AI ChatRobot</strong><br> こんにちは、何かお手伝いできますか?',type='messages')
+        msg = gr.Textbox(placeholder='質問を入力してください！', elem_classes='feedback', elem_id='bgc')
+        clear = gr.ClearButton(value='会話の記録をクリア', components=[msg, chatbot])
 
 
-    msg.submit(do_user, [msg, chatbot], [msg, chatbot], queue=False).then(do_it, chatbot, chatbot)
+        msg.submit(do_user, [msg, chatbot], [msg, chatbot], queue=False).then(do_it, chatbot, chatbot)
 
 
-instance.queue()
-instance.launch(server_name='127.0.0.1', server_port=8008,share=True)
+    instance.queue()
+    instance.launch(server_name='127.0.0.1', server_port=8008,share=True)
+
+def init():
+    # 初期化AI Robot
+    save_vectors_db()
+    global robot
+    robot = init_chain()
+
+
+if __name__ == '__main__':
+    # 初期化AI Robot
+    init()
+    run_gradio()
--- vectors_retrieval.py
+++ vectors_retrieval.py
@@ -1,8 +1,14 @@
 import os.path
+from platform import system
 
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.question_answering.map_reduce_prompt import system_template
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_text_splitters import CharacterTextSplitter
+from openai import models
 
 DB_DIR = 'faiss_db/'
 EMBEDDINGS = OpenAIEmbeddings()
@@ -11,21 +17,21 @@ def save_vectors_db():
     """ ベクターデータベースの構築 """
 
     if os.path.exists(DB_DIR):
-        print('ベクトルデータベースは既に存在します。')
+        print('ベクトルデータベースは既に存在します。再構築不要！')
     else:
         with open('datas.txt',encoding = 'utf8') as f:
-            content = f.read()
+            contents = f.read()
 
     #
     txt_splitter = CharacterTextSplitter(
-        separator=r'\d+.',
+        separator=r'\d+\.\n',
         is_separator_regex= True,
         chunk_size=100,
         chunk_overlap=0,
         length_function=len
     )
 
-    docs = txt_splitter.create_documents([content])
+    docs = txt_splitter.create_documents([contents])
     db = FAISS.from_documents(docs,EMBEDDINGS)
     db.save_local(DB_DIR)
 
@@ -36,10 +42,30 @@ def save_vectors_db():
 
 def init_chain():
 
-    # 1
+    # step_1 Loadベクターデータベース
     db = FAISS.load_local(DB_DIR,EMBEDDINGS,allow_dangerous_deserialization= True)
-    # 2
+    # step_2 プロンプトテンプレートの作成
+    system_prompt= """  
+    {context}
+    """
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            ('system',system_prompt),
+            ('human','{input}')
+        ]
+    )
+
+    # step_3 chain作成
+    # 検索
+    retriever = db.as_retriever(search_type='similarity_score_threshold', search_kwargs={"score_threshold": 0.7})
+
+
+    model = ChatOpenAI(model_name='gpt-4o', temperature = 0.2)
+    chain_1 = create_stuff_documents_chain(llm=model, prompt=prompt_template)
+    return create_retrieval_chain(retriever= retriever, combine_docs_chain= chain_1)
 
 
 if __name__ == '__main__':
     save_vectors_db()
+    chain = init_chain()
+    res = chain.invoke({'input':''})
